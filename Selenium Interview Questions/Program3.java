05310356dd74317d7b6515fa83e82b3c119e5824
Yadagiri Reddy <31485158+YadaGiriReddy@users.noreply.github.com>
2020-09-16 15:07:14 +0530

    Update Program3.java

--- Selenium Interview Questions/Program3.java
+++ Selenium Interview Questions/Program3.java
@@ -1,8 +1,13 @@
 package com.SeleniumInterviewQuestions;
 
 import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 import java.net.URL;
+import java.net.URLConnection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -17,24 +22,36 @@ public class Program3 {
 	public static void main(String[] args) throws Exception {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.theworldsworstwebsiteever.com/");
+		driver.get("https://www.hyrtutorials.com/");
 		Thread.sleep(5000);
+
+		Set<String> brokenlinkUrls = new HashSet<String>();
 		List<WebElement> links = driver.findElements(By.tagName("a"));
+		System.out.println(links.size());
+
 		for (WebElement link : links) {
-			String linkURL = link.getAttribute("href");
-			try {
-				URL url = new URL(linkURL);
-				HttpURLConnection httpURLConnection=(HttpURLConnection)url.openConnection();
-				httpURLConnection.setConnectTimeout(3000);
-				httpURLConnection.connect();
-				if(httpURLConnection.getResponseCode()==200)
-					System.out.println(linkURL+" - "+httpURLConnection.getResponseMessage());
-				else
-					System.err.println(linkURL+" - "+httpURLConnection.getResponseMessage());
-			} catch (Exception e) {
-				System.err.println(linkURL);
+			String linkURL = link.getAttribute("href");	
+			Proxy proxy = new Proxy(java.net.Proxy.Type.HTTP, new InetSocketAddress("hostname", 80));
+			URL url = new URL(linkURL);
+			URLConnection urlConnection = url.openConnection(proxy);
+			HttpURLConnection httpURLConnection = (HttpURLConnection) urlConnection;
+			httpURLConnection.setConnectTimeout(5000);
+			httpURLConnection.connect();
+			if(httpURLConnection.getResponseCode() != 200)
+			{
+				brokenlinkUrls.add(linkURL);
 			}
+//				System.out.println(linkURL + " - " + httpURLConnection.getResponseMessage());
+//			else
+//				System.err.println(linkURL + " - " + httpURLConnection.getResponseCode() + " - " + httpURLConnection.getResponseMessage());
+			
+			httpURLConnection.disconnect();
 		}
+
 		driver.quit();
+		
+		for (String brokenLinkUrl : brokenlinkUrls) {
+			System.err.println(brokenLinkUrl);
+		}
 	}
 }
