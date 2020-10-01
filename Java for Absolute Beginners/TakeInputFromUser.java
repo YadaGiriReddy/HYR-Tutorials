664320c4285f97135a973115e07142946f2c8cc6
Yadagiri Reddy <hyadagirireddy@gmail.com>
2020-10-01 18:53:30 +0530

    Getting user input data in java using scanner class

---
+++ Java for Absolute Beginners/TakeInputFromUser.java
@@ -0,0 +1,19 @@
+package practicePrograms;
+
+import java.util.Scanner;
+
+public class TakeInputFromUser {
+
+	public static void main(String[] args) {
+//		int i = 10;
+//		System.out.println(i);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("What is your name?");
+		String name = sc.nextLine();
+		System.out.println("Hello "+name);
+		System.out.println("What is your age?");
+		int age = sc.nextInt();
+		System.out.println("Your age is "+age);
+		sc.close();
+	}
+}
