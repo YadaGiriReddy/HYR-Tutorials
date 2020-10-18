b31e749bd234d98431dfa738e2c35019b6d22f17
Yadagiri Reddy <hyadagirireddy@gmail.com>
2020-10-19 00:40:23 +0530

    Switch satement program is added

---
+++ Java for Absolute Beginners/DecisionMakingStatements2.java
@@ -0,0 +1,34 @@
+package practicePrograms;
+
+public class DecisionMakingStatements2 {
+	public static void main(String[] args) {
+		int dayNumber = 2;
+		switch (dayNumber) {
+		case 1:
+			System.out.println("Monday");
+			break;
+		case 2:
+			System.out.println("Tueday");
+			break;
+		case 3:
+			System.out.println("Wedday");
+			break;
+		case 4:
+			System.out.println("Thuday");
+			break;
+		case 5:
+			System.out.println("Friday");
+			break;
+		case 6:
+			System.out.println("Satday");
+			break;
+		case 7:
+			System.out.println("Sunday");
+			break;
+		default:
+			System.err.println("Day number is invalid");
+			break;
+		}
+
+	}
+}
