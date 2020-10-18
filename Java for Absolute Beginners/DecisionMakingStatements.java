fc9680b70439638c21768e9849f3951149f8223d
Yadagiri Reddy <hyadagirireddy@gmail.com>
2020-10-19 00:39:38 +0530

    if and if-else program is added

---
+++ Java for Absolute Beginners/DecisionMakingStatements.java
@@ -0,0 +1,25 @@
+package practicePrograms;
+
+public class DecisionMakingStatements {
+	/*
+	Write a program to verify the eligibility of a student
+	to get the admission into a college based on his/her
+	entrance examination score.
+	*/
+	public static void main(String[] args) {
+		int securedMarks = 35;
+		int cutOffScore = 35;
+		
+		if(cutOffScore < securedMarks) {
+			System.out.println("Eligible");
+		}
+		else if(cutOffScore == securedMarks) {
+			System.out.println("Waiting list");
+		}
+		else
+		{
+			System.out.println("In-eligible");
+		}		
+		
+	}
+}
