c8da0353a757dfcecbd77af5077814222b0836d1
Yadagiri Reddy <hyadagirireddy@gmail.com>
2021-05-04 16:07:16 +0530

    Selenium Java in Telugu Course updated

--- README.md
+++ README.md
@@ -96,6 +96,7 @@ You can find the GitHub link in the video description or you can directly browse
 9. [How to handle TextBoxes in Selenium WebDriver - Part 8 | తెలుగు |](https://youtu.be/nZcqUKrsFMQ)
 10. [How to handle basic html controls in Selenium WebDriver - Part 9 | తెలుగు |](https://youtu.be/H-Agz7gOrdM)
 11. [How to handle Dropdowns in Selenium WebDriver - Part 10 | తెలుగు |](https://youtu.be/jHwDhenftfE)
+12. [Navigation Methods in Selenium WebDriver - Part 11 | తెలుగు |](https://youtu.be/2JQ_6swL28U)
 
 ***
 
---
+++ Selenium Java in Telugu/NavigationMethods.java
@@ -0,0 +1,34 @@
+package com.SeleniumPractice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/**
+ * @author Yadagiri Reddy
+ * Navigation Methods in Selenium WebDriver
+ */
+public class NavigationMethods {
+	public static void main(String[] args) throws Exception {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		Thread.sleep(3000);
+		driver.findElement(By.name("q")).sendKeys("HYR Tutorials");
+		driver.findElement(By.name("q")).submit();
+		Thread.sleep(3000);
+		driver.navigate().to("https://www.facebook.com/");
+		Thread.sleep(3000);
+		driver.findElement(By.id("email")).sendKeys("test@gmail.com");
+		Thread.sleep(3000);
+		driver.navigate().refresh();
+		Thread.sleep(3000);
+		driver.navigate().back();
+		Thread.sleep(3000);
+		driver.navigate().forward();
+		Thread.sleep(3000);
+		driver.quit();
+	}
+}
