8e1f23d262d99e6d2c2443e5aad869aaf4b906b0
Yadagiri Reddy <hyadagirireddy@gmail.com>
2021-05-18 12:09:05 +0530

    Implicit Wait Timeout Program added

---
+++ Selenium Java in Telugu/TestImplicitWaitTimeout.java
@@ -0,0 +1,26 @@
+package com.SeleniumPractice;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/**
+ * @author Yadagiri Reddy
+ * ImplicitWaitTimeout in Selenium WebDriver?
+ */
+public class TestImplicitWaitTimeout {
+	public static void main(String[] args) throws Exception {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().setSize(new Dimension(1000, 1000));
+		
+		driver.get("https://www.hyrtutorials.com/p/waits-demo.html");
+		driver.findElement(By.id("btn1")).click();
+		driver.findElement(By.id("txt1")).sendKeys("HYR");
+		
+	}
+}
